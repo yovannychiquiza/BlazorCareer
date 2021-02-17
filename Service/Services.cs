90717faa653a654ebd7c4d46fef7de5246d463b0
yovannychiquiza <yovannychiquiza@gmail.com>
2021-02-17 17:10:20 -0500

    Add country model

---
+++ Service/Services.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace BlazorCareer.Client.Service
+{
+    public static class Services
+    {
+        public static string uri = "http://localhost:5000/api/careercloud/system/v1/countrycode";
+        
+    }
+}
